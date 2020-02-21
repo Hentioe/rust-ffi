f27e7500f6400df66f888a221d3f03b8a038eb9d
Hentioe <me@bluerain.io>
2020-02-21 10:43:00 -0400

    Update bindings

--- dart/libcore.dart
+++ dart/libcore.dart
@@ -5,17 +5,19 @@ final dylib = DynamicLibrary.open('lib/libcore.so');
 
 typedef hello_func = Void Function();
 typedef str_func = Pointer<Utf8> Function();
-typedef echo_func = Pointer<Utf8> Function(Pointer<Utf8> str);
+typedef echo_func = Void Function(Pointer<Utf8> str);
 
 typedef Hello = void Function();
-typedef Echo = Pointer<Utf8> Function(Pointer<Utf8> str);
+typedef Echo = void Function(Pointer<Utf8> str);
 
 final Hello call_hello = dylib
    .lookup<NativeFunction<hello_func>>('hello')
    .asFunction();
 
-final strPointer = dylib.lookup<NativeFunction<str_func>>('str');
-final call_str = strPointer.asFunction<str_func>();
+final call_str = dylib
+  .lookup<NativeFunction<str_func>>('str')
+  .asFunction<str_func>();
 
-final echoPointer = dylib.lookup<NativeFunction<echo_func>>('echo');
-final call_echo = echoPointer.asFunction<echo_func>();
+final Echo call_echo = dylib
+  .lookup<NativeFunction<echo_func>>('echo')
+  .asFunction();
